0f4806e30c2b51a24a621a432ec1b5563995259d
Ross Taylor-Turner <ross.turner001@googlemail.com>
2021-08-11 23:11:31 +0100

    Check for existing discord avatar

--- frontend/src/player/PlayerAvatar.js
+++ frontend/src/player/PlayerAvatar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 
 const PlayerAvatar = ({loggedInPlayer, size}) => {
-    if (!loggedInPlayer) {
+    if (!loggedInPlayer || !loggedInPlayer.discordAvatar) {
         return null;
     } else {
         return <Image avatar size={ size ? size : 'medium'} src={'https://cdn.discordapp.com/avatars/'+loggedInPlayer.discordId+'/'+loggedInPlayer.discordAvatar+'.png'} />;
