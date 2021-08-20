6692dcbe50a262493aa388bbcf4792b2d5696c66
Ross Taylor-Turner <ross.turner001@googlemail.com>
2021-08-20 20:21:25 +0100

    Changed from BrowserRouter to HashRouter to fix issues with browser refresh on live site

--- frontend/src/AppRouterWrapper.js
+++ frontend/src/AppRouterWrapper.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import App from "./App";
 
 const AppRouterWrapper = () => {
