447dc9d7025fe976e2d47d9ce754559426a0a9a7
Ross Taylor-Turner <ross.turner001@googlemail.com>
2021-08-31 16:42:42 +0100

    Changed weighting of map type to favour pangae-like maps, not continents-like maps

--- src/main/java/technology/rocketjump/civimperium/mapgen/MapType.java
+++ src/main/java/technology/rocketjump/civimperium/mapgen/MapType.java
@@ -5,13 +5,13 @@ import static technology.rocketjump.civimperium.mapgen.MapType.MapLandAmount.*;
 public enum MapType implements Weightable {
 
 	Archipelago(1f, Light),
-	Continents(8f, Average),
-	Continents_and_Islands(6f, Average),
+	Continents(2f, Average),
+	Continents_and_Islands(1f, Average),
 	Fractal(1f, Average),
-	Highlands(3f, VeryHeavy),
-	Island_Plates(4f, Light),
-	Lakes(2f, VeryHeavy),
-	Pangaea(8f, Heavy),
+	Highlands(4f, VeryHeavy),
+	Island_Plates(2f, Light),
+	Lakes(8f, VeryHeavy),
+	Pangaea(9f, Heavy),
 	Primordial(3f, Heavy),
 
 	// Real world maps, split between a weighting of 1
@@ -21,10 +21,10 @@ public enum MapType implements Weightable {
 	Europe(0.25f, Heavy),
 	Mediterranean_Large(0.25f, Heavy),
 
-	Seven_Seas(5f, Heavy),
+	Seven_Seas(7f, Heavy),
 	Shuffle(2f, Average),
 	Small_Continents(2f, Average),
-	Terra(2f, Light),
+	Terra(4f, Light),
 	Wetlands(2f, Average);
 
 	public final float weighting;
